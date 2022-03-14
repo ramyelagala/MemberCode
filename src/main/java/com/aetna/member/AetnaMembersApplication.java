67afd596ce0e248ff3fd26436f0f009c4c3d0899
ramyelagala <ramyelagala@gmail.com>
2022-03-14 16:55:11 +0530

    remove spring security

--- src/main/java/com/aetna/member/AetnaMembersApplication.java
+++ src/main/java/com/aetna/member/AetnaMembersApplication.java
@@ -3,11 +3,9 @@ package com.aetna.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableWebSecurity
 public class AetnaMembersApplication {
 
 	public static void main(String[] args) {
